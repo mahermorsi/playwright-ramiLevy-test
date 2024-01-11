b9e07aa2692be71329d7223c10416527770aed3f
mahermorsi <mahermorsi@gmail.com>
2024-01-11 09:32:52 +0200

    added response wait timeout

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,7 +2,6 @@
 import { test as setup } from '@playwright/test';
 import { LoginPage } from '../logic/pages/login-page';
 import {configJson} from '../config.json';
-import { MainPage } from '../logic/pages/main-page';
 
 const authFile = 'playwright/.auth/user.json';
 
@@ -10,6 +9,7 @@ setup('authenticate', async ({page}) => {
     await page.goto(configJson.url)
     const loginPage = new LoginPage(page);
     await loginPage.fullLoginProcess(configJson.user, configJson.password);
-    await page.waitForTimeout(5000)
+    //await page.waitForTimeout(5000)
+    await page.waitForResponse(response => response.url().endsWith('/login') && response.status() === 200)
     await page.context().storageState({ path: authFile });
 });
